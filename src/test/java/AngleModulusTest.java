4c3a74e60b9208cb64c689612d26fe7a1b4aa5a8
Kevin Yu <kevinyu567@gmail.com>
2022-07-07 22:57:04 -0700

    Write more tests

--- src/test/java/AngleModulusTest.java
+++ src/test/java/AngleModulusTest.java
@@ -7,33 +7,119 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.subsystems.swerve.SwerveModule;
 
 public class AngleModulusTest {
+    private static final double ACCEPTABLE_ANGLE_DELTA = 1e-15;
+
     /**
-     * This test ensures that the optimize method is able to correctly perform angle
-     * wraparound (going from pi to 7pi/6 when the target angle is -5pi/6) without
-     * optimization; in this case the delta should be less than pi/2.
+     * This test ensures that the optimize method correctly handles the case where the
+     * target angle and current angle are zero (delta = 0).
+     * 
+     * Current angle: 0, target angle: 0
+     */
+    @Test
+    public void bothZero() {
+        SwerveModuleState target = new SwerveModuleState(1.0, new Rotation2d(0));
+        var optimized = SwerveModule.optimizeModuleState(target, new Rotation2d(0));
+
+        // Velocity: 1.0 (not flipped), angle: 0
+        assertEquals(1.0, optimized.getFirst(), 0);
+        assertEquals(0, optimized.getSecond(), ACCEPTABLE_ANGLE_DELTA);
+    }
+
+    /**
+     * This test ensures that the optimize method correctly handles the case where the
+     * target angle and current angle are the same (delta = 0).
+     * 
+     * Current angle: pi/6, target angle: pi/6
+     */
+    @Test
+    public void bothSame() {
+        SwerveModuleState target = new SwerveModuleState(1.0, new Rotation2d(Math.PI / 6.0));
+        var optimized = SwerveModule.optimizeModuleState(target, new Rotation2d(Math.PI / 6.0));
+
+        // Velocity: 1.0 (not flipped), angle: pi/6
+        assertEquals(1.0, optimized.getFirst(), 0);
+        assertEquals(Math.PI / 6.0, optimized.getSecond(), ACCEPTABLE_ANGLE_DELTA);
+    }
+
+    /**
+     * This test ensures that the optimize method correctly optimizes the module state
+     * when the delta is greater than 90 degrees.
+     * 
+     * Current angle: pi/6, target angle: 5pi/6
+     */
+    @Test
+    public void optimizedNowraparound() {
+        SwerveModuleState target = new SwerveModuleState(1.0, new Rotation2d(5 * Math.PI / 6.0));
+        var optimized = SwerveModule.optimizeModuleState(target, new Rotation2d(Math.PI / 6.0));
+
+        // Velocity: -1.0 (flipped), angle: -pi/6
+        assertEquals(-1.0, optimized.getFirst(), 0);
+        assertEquals(-Math.PI / 6.0, optimized.getSecond(), ACCEPTABLE_ANGLE_DELTA);
+    }
+
+    /**
+     * This test ensures that the optimize method correctly performs angle wraparound 
+     * (ie. going from pi to 7pi/6 when the target angle is -5pi/6) without optimization.
+     * 
+     * Current angle: 5pi/6, target angle: -5pi/6
      */
     @Test
     public void unoptimizedWraparound() {
         SwerveModuleState target = new SwerveModuleState(1.0, new Rotation2d(-5 * Math.PI / 6.0));
-        var optimized = SwerveModule.optimizeModuleState(target, new Rotation2d(29 * Math.PI / 6.0));
+        var optimized = SwerveModule.optimizeModuleState(target, new Rotation2d(5 * Math.PI / 6.0));
 
-        // Velocity: 1.0 (not flipped), angle: 29pi/6 + pi/3 (delta)
+        // Velocity: 1.0 (not flipped), angle: 5pi/6 + pi/3 (delta) = 7pi/6
         assertEquals(1.0, optimized.getFirst(), 0);
-        assertEquals(31 * Math.PI / 6.0, optimized.getSecond(), 0);
+        assertEquals(7 * Math.PI / 6.0, optimized.getSecond(), ACCEPTABLE_ANGLE_DELTA);
     }
 
     /**
-     * This test ensures that the optimize method is able to correctly perform angle
-     * wraparound (going from pi to 7pi/6 when the target angle is -5pi/6) while also performing
-     * optimization; in this case the delta should be greater than pi/2.
+     * This test ensures that the optimize method correctly optimizes the module state
+     * with angle wraparound (ie. going from pi to 7pi/6 when the target angle is -5pi/6).
+     * 
+     * Current angle: pi/3, target angle: -5pi/6
      */
     @Test
     public void optimizedWraparound() {
         SwerveModuleState target = new SwerveModuleState(1.0, new Rotation2d(-5 * Math.PI / 6.0));
+        var optimized = SwerveModule.optimizeModuleState(target, new Rotation2d(Math.PI / 3.0));
+
+        // Velocity: -1.0 (flipped), angle: pi/3 - pi/6 (delta) = pi/6
+        assertEquals(-1.0, optimized.getFirst(), 0);
+        assertEquals(Math.PI / 6.0, optimized.getSecond(), ACCEPTABLE_ANGLE_DELTA);
+    }
+
+    /**
+     * This test ensures that the optimize method both correctly performs angle wraparound 
+     * (ie. going from pi to 7pi/6 when the target angle is -5pi/6) without optimization and 
+     * correctly applies the delta to an angle outside of (-pi, pi].
+     * 
+     * Current angle: 29pi/6, target angle: -5pi/6
+     */
+    @Test
+    public void unoptimizedUnboundedWraparound() {
+        SwerveModuleState target = new SwerveModuleState(1.0, new Rotation2d(-5 * Math.PI / 6.0));
+        var optimized = SwerveModule.optimizeModuleState(target, new Rotation2d(29 * Math.PI / 6.0));
+
+        // Velocity: 1.0 (not flipped), angle: 29pi/6 + pi/3 (delta) = 31pi/6
+        assertEquals(1.0, optimized.getFirst(), 0);
+        assertEquals(31 * Math.PI / 6.0, optimized.getSecond(), ACCEPTABLE_ANGLE_DELTA);
+    }
+
+    /**
+     * This test ensures that the optimize method both correctly optimizes the module state
+     * with angle wraparound (ie. going from pi to 7pi/6 when the target angle is -5pi/6) and 
+     * correctly applies the delta to an angle outside of (-pi, pi].
+     * 
+     * Current angle: 26pi/6, target angle: -5pi/6
+     */
+    @Test
+    public void optimizedUnboundedWraparound() {
+        SwerveModuleState target = new SwerveModuleState(1.0, new Rotation2d(-5 * Math.PI / 6.0));
         var optimized = SwerveModule.optimizeModuleState(target, new Rotation2d(26 * Math.PI / 6.0));
 
-        // Velocity: -1.0 (flipped), angle: 26pi/6 - pi/6 (delta)
+        // Velocity: -1.0 (flipped), angle: 26pi/6 - pi/6 (delta) = 25pi/6
         assertEquals(-1.0, optimized.getFirst(), 0);
-        assertEquals(25 * Math.PI / 6.0, optimized.getSecond(), 0);
+        assertEquals(25 * Math.PI / 6.0, optimized.getSecond(), ACCEPTABLE_ANGLE_DELTA);
     }
 }
