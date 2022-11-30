e997a267b7ac4688a8eb66c7b5d7864339072506
Kevin Yu <60120929+ky28059@users.noreply.github.com>
2022-11-29 17:19:04 -0800

    Gson JSON deserialization

--- src/main/java/frc/robot/jetson/JetsonConnection.java
+++ src/main/java/frc/robot/jetson/JetsonConnection.java
@@ -1,25 +1,35 @@
 package frc.robot.jetson;
 
+import com.google.gson.Gson;
+
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 /**
- * A ZeroMQ socket connection to the jetson. Binds to a static port on the RIO, sends data to the jetson 
- * via REP, and receives data via REQ.
+ * A ZeroMQ socket connection to the jetson. The connection binds to a static port on the RIO, sends data to the 
+ * jetson via REP, and receives JSON data via REQ.
  */
 public class JetsonConnection extends Thread {
+    private final Gson gson = new Gson();
+
+    private static final String SERVER_IP = "tcp://10.1.92.12:5000";
+
     @Override
     public void run() {
         try (ZContext context = new ZContext()) {
             // ZeroMQ reply server bound to static IP, jetson connects via REQ.
+            System.out.println("Connecting a REP server to " + SERVER_IP);
             ZMQ.Socket socket = context.createSocket(SocketType.REP);
-            socket.bind("tcp://10.1.92.12:5000");
+            socket.bind(SERVER_IP);
 
             // While the thread is still running, keep waiting for messages from the jetson.
             while (!Thread.currentThread().isInterrupted()) {
                 String message = socket.recvStr(0);
                 System.out.println(message);
+
+                JetsonData data = gson.fromJson(message, JetsonData.class);
+                System.out.println(data.x + " " + data.y + " " + data.z);
 
                 Thread.sleep(1000);
 
---
+++ src/main/java/frc/robot/jetson/JetsonData.java
@@ -0,0 +1,10 @@
+package frc.robot.jetson;
+
+/**
+ * The JSON data object sent by the jetson.
+ */
+public class JetsonData {
+    public double x;
+    public double y;
+    public double z;
+}
