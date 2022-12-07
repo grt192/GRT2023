20cc46241d24780dedf69a7268e44823e46e3037
Rishay Jain <rishayjain@Rishays-MacBook-Air.local>
2022-12-07 15:31:15 -0800

    PRIME ended and it doesn't work

--- src/main/java/frc/robot/subsystems/Carriage.java
+++ src/main/java/frc/robot/subsystems/Carriage.java
@@ -16,6 +16,7 @@ import static frc.robot.Constants.CarriageConstants.TOP_SERVO;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -25,7 +26,7 @@ public class Carriage extends SubsystemBase {
     public boolean liftCarriage = false;  // tracks whether carriage should be lifted
 
     DoubleSolenoid piston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, CARRIAGE_SOLENOID_F,CARRIAGE_SOLENOID_R); // lifts carriage
-    
+    Timer timer = new Timer();
     Servo top = new Servo(TOP_SERVO); // top servo opening door
     Servo bottom = new Servo(BOTTOM_SERVO); // bottom servo opening door
 
@@ -37,21 +38,34 @@ public class Carriage extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
 
-    // if the button is pressed, toggle the piston using the class toggle method (didn't want to pass Value to set method)
-    if(liftCarriage){
-      liftCarriage = false; 
+    
+    if(liftCarriage){ 
+      top.setAngle(TOP_CLOSED); // if set to lift carriage, first close the doors
+      bottom.setAngle(BOTTOM_CLOSED);
+      timer.reset(); // reset and start timer to make sure doors are closed before lift
+      timer.start();
+      liftCarriage = false; // set to false so we don't keep resetting timer
+      
+    }
+    else{ // if not to lift carriage, set the door to be open
+      top.setAngle(TOP_OPEN);
+      bottom.setAngle(BOTTOM_OPEN);
+    }
+
+    if(timer.get() >= 0.2){ //if time elapsed meets criterion, 
+      timer.stop();
+      timer.reset();
       piston.toggle();
     }
-    
 
-    // if(openDoor){
-    //     top.set(TOP_OPEN);
-    //     bottom.set(BOTTOM_OPEN);
-    // }
-    // else{
-    //     top.set(TOP_CLOSED);
-    //     bottom.set(BOTTOM_CLOSED);
-    // }
+    if(openDoor){
+        top.setAngle(TOP_OPEN);
+        bottom.setAngle(BOTTOM_OPEN);
+    }
+    else{
+        top.setAngle(TOP_CLOSED);
+        bottom.setAngle(BOTTOM_CLOSED);
+    }
 
     
   }
