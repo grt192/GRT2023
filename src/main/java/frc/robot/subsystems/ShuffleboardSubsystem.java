1feb355a0db3ad3c491158b4ae9ce93135ef1823
CrolineCrois <maoninii@gmail.com>
2023-01-11 16:06:56 -0800

    field and moving robot on field

--- src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
@@ -1,9 +1,15 @@
 package frc.robot.subsystems;
 
+import java.text.FieldPosition;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,12 +21,19 @@ public class ShuffleboardSubsystem extends SubsystemBase {
     private final GenericEntry bentry = tab.add("Toggle", false).getEntry();
     private boolean bool = false;
 
+    private final Field2d cfield = new Field2d();
+    private double x = 0;
+    private double y = 0;
+    private final Rotation2d ratot = Rotation2d.fromDegrees(20);
+
     // ClassName
     // variableName
     // CONSTANT_NAME
 
     public ShuffleboardSubsystem() {
         tab.add(new InstantCommand(this::callback, this));
+        SmartDashboard.putData("Field", cfield);
+        
     }
 
     private void callback(){
@@ -28,10 +41,16 @@ public class ShuffleboardSubsystem extends SubsystemBase {
         System.out.println("hja");
     }
 
+
     @Override
     public void periodic() {
         double offset = gentry.getDouble(0.0);
         entry.setValue(Timer.getFPGATimestamp() + offset);
         bentry.setValue(bool);
+        //cfield.getObject("grobot");
+        cfield.setRobotPose(x, y, ratot);
+        x += .01;
+        y += .01;
+        
     }
 }
