8a0fb21ccf6cd1d7f7d0baac8913a4355074eec0
Rishay Jain <rishayjain@Rishays-MacBook-Air.local>
2023-01-14 12:33:38 -0800

    balancing code and fixing class capitalization

--- src/main/java/frc/robot/commands/BalancerCommand.java
+++ src/main/java/frc/robot/commands/BalancerCommand.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
 import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
@@ -14,13 +16,17 @@ public class BalancerCommand extends CommandBase {
   double currentAngle = 0; // current pitch angle
   double oldAngle = 0;
   double deltaAngle;
-  double gain = 0.01; // gain for controller
+  double gain = 0.02; // gain for controller
   double initialHeading;
+
+  Timer balanceTimer;
+  boolean timerStarted;
 
   private AHRS ahrs = driveTrain.ahrs; 
 
   public BalancerCommand(Drivetrain subsystem) {
     driveTrain = subsystem;
+    balanceTimer = new Timer();
 
     // Use addRequirements() here to declare subsystem dependencies.
     // addRequirements(subsystem);
@@ -30,7 +36,7 @@ public class BalancerCommand extends CommandBase {
   @Override
   public void initialize() {
     initialHeading = ahrs.getCompassHeading();
-    System.out.println("balancer initialized");
+    System.out.println("---------------------- Balancer initialized ----------------------");
     oldAngle = -ahrs.getPitch();
     
   }
@@ -39,7 +45,7 @@ public class BalancerCommand extends CommandBase {
   @Override
   public void execute() {
     System.out.println("pitch");
-    System.out.println(-1*ahrs.getPitch());
+    System.out.println(-ahrs.getPitch());
 
     switch (phase) {
         case 1: // moving forward unless delta angle is NEGATIVE (robot pitches down a little bit once most of the mass is on the station)
@@ -50,7 +56,7 @@ public class BalancerCommand extends CommandBase {
                 phase ++; // increase the stage number if stage 1 completed and conditions met
             }
             else{ 
-                System.out.println("auton speed");
+                System.out.println("Moving towards station");
                 returnPower = 0.3; // drive robot slowly (towards the station)
                 oldAngle = currentAngle;
             }
@@ -59,28 +65,36 @@ public class BalancerCommand extends CommandBase {
             
             // proportional-ish controller
             currentAngle = -ahrs.getPitch();
+            
+            if(timerStarted){ // waits 1.0 seconds to see if it's really balanced --> stops if it is, goes back to balancing if it isn't
+              if(balanceTimer.get() >= 1.0){
+                if(Math.abs(currentAngle) <= 2.0){
+                  phase++;
+                  balanceTimer.stop();
+                  balanceTimer.reset();
+                  timerStarted = false;
+                  break;
+                }
+              }
+            }
+            
             if(Math.abs(currentAngle) <= 2.0){ // if the angle is within 2 degrees of level
+              if(!timerStarted){
+                balanceTimer.reset();
+                balanceTimer.start();
+                timerStarted = true;
+              }
               returnPower = 0.0;  
-              phase++;
             }
             else{
-                returnPower = gain * currentAngle; // the desired angle is level, so the current angle of the robot is inherently the error
-                Math.min(0.25,returnPower);
+                // the desired angle is level (0), so the current angle of the robot is inherently the error
+                returnPower = Math.min(0.25,gain*currentAngle);
             }
                         
             break;
         
-        case 3: // stops the robot if it thinks its level, then checks again
+        case 3: // stops robot
             returnPower = 0.0;
-            currentAngle = -ahrs.getPitch();
-            if(Math.abs(currentAngle) <= 2.0){ // if the angle is within 2 degrees of level
-              returnPower = 0.0;  
-              phase++;
-            }
-            else{
-                returnPower = gain * currentAngle; // the desired angle is level, so the current angle of the robot is inherently the error
-                Math.min(0.25,returnPower);
-            }
             break;
     
         default:
@@ -100,6 +114,6 @@ public class BalancerCommand extends CommandBase {
   @Override
   public boolean isFinished() {
     
-    return phase == 4;
+    return phase == 3;
   }
 }
