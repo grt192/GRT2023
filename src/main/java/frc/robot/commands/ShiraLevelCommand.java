741d3b263852345838060de8fcb8b533df476016
CrolineCrois <maoninii@gmail.com>
2023-01-19 19:46:45 -0800

    mover to different levels command

--- src/main/java/frc/robot/commands/ShiraLevelCommand.java
+++ src/main/java/frc/robot/commands/ShiraLevelCommand.java
@@ -1,5 +1,39 @@
 package frc.robot.commands;
 
-public class ShiraLevelCommand {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.MoverSubsystem;
+import frc.robot.subsystems.MoverSubsystem.MoverPosition;
+
+public class ShiraLevelCommand extends CommandBase{
+    private final MoverSubsystem moverSubsystem;
+    private final MoverPosition level;
+    private boolean done;
+
+    public ShiraLevelCommand(MoverSubsystem moverSubsystem, MoverPosition level){
+        done = false;
+        this.moverSubsystem = moverSubsystem;
+        this.level = level;
+    }
+    
+    @Override
+    public void initialize() {
+        System.out.println("Mover to " + level);
+    }
+
+    @Override
+    public void execute() {
+        moverSubsystem.setState(level);
+        done = true;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        System.out.println("Mover at " + level);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return done;
+    }
     
 }
