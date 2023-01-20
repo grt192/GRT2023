74baf5c798fda0e801d6313ac927c5f171fc9b15
Alina Li <alina.li@outlook.com>
2023-01-19 16:53:44 -0800

    Integrate photonvision into swerve pose estimator

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.drivetrain;
 
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
+import frc.robot.vision.PhotonWrapper;
 
 import static frc.robot.Constants.SwerveConstants.*;
 
@@ -10,14 +11,15 @@ public class SwerveSubsystem extends BaseSwerveSubsystem {
     public static final double MAX_ACCEL = 3; // Max robot tangential acceleration, in m/s^2; TODO: measure this
     public static final double MAX_OMEGA = MAX_VEL / tlPos.getNorm(); // Max robot angular velocity, in rads/s (omega = v / r)
 
-    public SwerveSubsystem() {
+    public SwerveSubsystem(PhotonWrapper photonWrapper) {
         super(
             new SwerveModule.TopLeft(tlDrive, tlSteer, tlOffsetRads),
             new SwerveModule.TopRight(trDrive, trSteer, trOffsetRads),
             new SwerveModule.BottomLeft(blDrive, blSteer, blOffsetRads),
             new SwerveModule.BottomRight(brDrive, brSteer, brOffsetRads),
             MAX_VEL, MAX_ACCEL, MAX_OMEGA, 
-            new SwerveDriveKinematics(tlPos, trPos, blPos, brPos)
+            new SwerveDriveKinematics(tlPos, trPos, blPos, brPos),
+            photonWrapper
         );
     }
 }
--- src/main/java/frc/robot/subsystems/drivetrain/SwerveSubsystem2020.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveSubsystem2020.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.drivetrain;
 
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import frc.robot.vision.PhotonWrapper;
 
 import static frc.robot.Constants.SwerveConstants2020.*;
 
@@ -9,14 +10,15 @@ public class SwerveSubsystem2020 extends BaseSwerveSubsystem {
     public static final double MAX_ACCEL = 3; // Max robot tangential acceleration, in m/s^2
     public static final double MAX_OMEGA = Math.toRadians(60); // Max robot angular velocity, in rads/s
 
-    public SwerveSubsystem2020() {
+    public SwerveSubsystem2020(PhotonWrapper photonWrapper) {
         super(
             new SwerveModule2020(tlDrive, tlSteer, tlOffsetRads),
             new SwerveModule2020(trDrive, trSteer, trOffsetRads),
             new SwerveModule2020(blDrive, blSteer, blOffsetRads),
             new SwerveModule2020(brDrive, brSteer, brOffsetRads),
             MAX_VEL, MAX_ACCEL, MAX_OMEGA,
-            new SwerveDriveKinematics(tlPos, trPos, blPos, brPos)
+            new SwerveDriveKinematics(tlPos, trPos, blPos, brPos),
+            photonWrapper
         );
     }
 }
