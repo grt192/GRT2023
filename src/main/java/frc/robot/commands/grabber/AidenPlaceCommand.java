46255a404c8db234615b17b9b07a8e87bdc0a55e
CrolineCrois <maoninii@gmail.com>
2023-02-04 14:42:56 -0800

    update aiden place command

--- src/main/java/frc/robot/commands/grabber/AidenPlaceCommand.java
+++ src/main/java/frc/robot/commands/grabber/AidenPlaceCommand.java
@@ -6,12 +6,13 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.RollerSubsystem;
 
 /**
- * [OUTDATED] Places 1 game piece with the roller mech. This command runs the rollers in reverse
+ * [NOT OUTDATED ANYMORE AYYYY] Places 1 game piece with the roller mech. This command runs the rollers in reverse
  * at a set power for 2 seconds.
  */
 public class AidenPlaceCommand extends CommandBase {
     private final RollerSubsystem rollerSubsystem;
     private final Timer timer;
+    private boolean done;
 
     public AidenPlaceCommand(RollerSubsystem rollerSubsystem){
         this.rollerSubsystem = rollerSubsystem;
@@ -23,12 +24,20 @@ public class AidenPlaceCommand extends CommandBase {
     @Override
     public void initialize() {
         System.out.println("Roller to place");
-        timer.start();
     } 
 
     @Override
     public void execute() {
-        rollerSubsystem.setRollPower(-0.2);
+        //open motor to drop
+        rollerSubsystem.openMotor();
+        //if limit switch is still pressed (piece is still in)
+        if (!rollerSubsystem.isLimit()){
+            timer.start();
+            rollerSubsystem.setRollPower(-0.1);
+        }
+        else{
+            done = true;
+        }
     }
 
     @Override
@@ -39,6 +48,6 @@ public class AidenPlaceCommand extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return timer.hasElapsed(2);
+        return (timer.hasElapsed(2) || done);
     }
 }
--- src/main/java/frc/robot/commands/grabber/MatthewPlaceCommand.java
+++ src/main/java/frc/robot/commands/grabber/MatthewPlaceCommand.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.GripperSubsytem;
 
 /**
- * Places 1 game piece with the pfft gripper mech.
+ * [no use] Places 1 game piece with the pfft gripper mech.
  */
 public class MatthewPlaceCommand extends InstantCommand {
     public MatthewPlaceCommand(GripperSubsytem gripperSubsystem) {
