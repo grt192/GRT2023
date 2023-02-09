ca718a1188ef873cb98cbd41a1b8dc9e3b16e7ef
CrolineCrois <maoninii@gmail.com>
2023-02-09 15:35:31 -0800

    crolor sensor actually to main this time

--- src/main/java/frc/robot/subsystems/RollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/RollerSubsystem.java
@@ -2,9 +2,12 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.motorcontrol.MotorUtil;
@@ -19,6 +22,30 @@ public class RollerSubsystem extends SubsystemBase {
     public final DigitalInput limitSwitch;
 
     private final Timer openTimer = new Timer();
+
+    private final I2C.Port sensorPort = I2C.Port.kMXP;
+    private final ColorSensorV3 crolorSensor = new ColorSensorV3(sensorPort);
+
+    private final int coneRedMax = 80;
+    private final int coneRedMin = 65;
+    private final int coneGreenMax = 140;
+    private final int coneGreenMin = 128;
+    private final int coneBlueMax = 51;
+    private final int coneBlueMin = 45;
+
+    private final int cubeRedMax = 65;
+    private final int cubeRedMin = 55;
+    private final int cubeGreenMax = 120;
+    private final int cubeGreenMin = 108;
+    private final int cubeBlueMax = 85;
+    private final int cubeBlueMin = 75;
+
+    //for tuning
+    // private final ShuffleboardTab tab = Shuffleboard.getTab("Color");
+    // private final GenericEntry entry; 
+    // private final GenericEntry gentry;
+    // private final GenericEntry bentry;
+
     private final double OPEN_TIME_SECONDS = 1.0;
     private final double COOLDOWN_SECONDS = 2.0;
 
@@ -38,6 +65,12 @@ public class RollerSubsystem extends SubsystemBase {
         openMotor.setInverted(true);
 
         limitSwitch = new DigitalInput(LIMIT_SWITCH_ID);
+
+        //for tuning
+        // entry = tab.add("R", 0).getEntry();
+        // gentry = tab.add("G", 0).getEntry();
+        // bentry = tab.add("B", 0).getEntry();
+
     }
 
     /**
@@ -81,5 +114,26 @@ public class RollerSubsystem extends SubsystemBase {
         } else {
             leftBeak.set(Math.min(rollPower, 0.0));
         }
+
+        Color detectedColor = crolorSensor.getColor();
+        double red = detectedColor.red * 255;
+        double green = detectedColor.green * 255;
+        double blue = detectedColor.blue * 255;
+
+        //for tuning
+        // entry.setValue(red);
+        // gentry.setValue(green);
+        // bentry.setValue(blue);
+
+        if (red > cubeRedMin && red < cubeRedMax && blue > cubeBlueMin && blue < cubeBlueMax && green < cubeGreenMax && green > cubeGreenMin){
+            System.out.println("cube");
+        }
+        else if (red > coneRedMin && red < coneRedMax && blue > coneBlueMin && blue < coneBlueMax && green < coneGreenMax && green > coneGreenMin){
+            System.out.println("cone");
+        }
+        else{
+            System.out.println("no object");
+        }
+        
     }
 }
