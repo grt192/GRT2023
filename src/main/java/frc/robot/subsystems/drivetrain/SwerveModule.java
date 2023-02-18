aaa0225ac2527ccd3b328a8eed502c9c641b48c9
Alina Li <alina.li@outlook.com>
2023-02-18 00:01:42 -0800

    Reshuffle util classes into folders

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
@@ -21,7 +21,6 @@ import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-
 import frc.robot.util.MotorUtil;
 
 /**
