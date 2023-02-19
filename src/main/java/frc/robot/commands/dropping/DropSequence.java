e4f835511cdbcdffd33df26a4037df84e642ba0e
penguin212 <dhuber06@gmail.com>
2023-02-18 17:17:43 -0800

    Dropper Sequences

---
+++ src/main/java/frc/robot/commands/dropping/DropSequence.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.dropping;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.mover.TiltedElevatorCommand;
+import frc.robot.subsystems.RollerSubsystem;
+import frc.robot.subsystems.TiltedElevatorSubsystem;
+import frc.robot.subsystems.TiltedElevatorSubsystem.ElevatorState;
+
+public class DropSequence extends SequentialCommandGroup{
+    
+    public DropSequence(
+        RollerSubsystem rollerSubsystem, TiltedElevatorSubsystem tiltedElevatorSubsystem, 
+        ElevatorState dropHeight, double waitTime1, double waitTime2
+    ) {
+
+        addRequirements(rollerSubsystem, tiltedElevatorSubsystem);
+
+        addCommands(
+            new TiltedElevatorCommand(tiltedElevatorSubsystem, dropHeight),
+            new WaitCommand(waitTime1),
+            new InstantCommand(rollerSubsystem::openMotor, rollerSubsystem),
+            new WaitCommand(waitTime2),
+            new TiltedElevatorCommand(tiltedElevatorSubsystem, ElevatorState.GROUND)
+        );
+    }
+    
+}
