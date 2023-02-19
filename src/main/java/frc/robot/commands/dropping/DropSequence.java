de765b00fb616398d1ecb5c06156acbcdf79c52f
Kevin Yu <kevinyu567@gmail.com>
2023-02-18 17:27:37 -0800

    Remove unnecessary layers of nesting

--- src/main/java/frc/robot/commands/dropping/DropSequence.java
+++ src/main/java/frc/robot/commands/dropping/DropSequence.java
@@ -3,18 +3,17 @@ package frc.robot.commands.dropping;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+
 import frc.robot.commands.mover.TiltedElevatorCommand;
 import frc.robot.subsystems.RollerSubsystem;
 import frc.robot.subsystems.TiltedElevatorSubsystem;
 import frc.robot.subsystems.TiltedElevatorSubsystem.ElevatorState;
 
 public class DropSequence extends SequentialCommandGroup{
-    
     public DropSequence(
         RollerSubsystem rollerSubsystem, TiltedElevatorSubsystem tiltedElevatorSubsystem, 
         ElevatorState dropHeight, double waitTime1, double waitTime2
     ) {
-
         addRequirements(rollerSubsystem, tiltedElevatorSubsystem);
 
         addCommands(
@@ -25,5 +24,4 @@ public class DropSequence extends SequentialCommandGroup{
             new TiltedElevatorCommand(tiltedElevatorSubsystem, ElevatorState.GROUND)
         );
     }
-    
 }
--- src/main/java/frc/robot/commands/dropping/DropperChooserCommand.java
+++ src/main/java/frc/robot/commands/dropping/DropperChooserCommand.java
@@ -1,26 +1,34 @@
 package frc.robot.commands.dropping;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 import frc.robot.subsystems.RollerSubsystem;
 import frc.robot.subsystems.TiltedElevatorSubsystem;
 import frc.robot.subsystems.TiltedElevatorSubsystem.ElevatorState;
 
-public class DropperChooserCommand{
-    public DropperChooserCommand(){
+public class DropperChooserCommand extends InstantCommand {
+    public DropperChooserCommand(RollerSubsystem rollerSubsystem, TiltedElevatorSubsystem tiltedElevatorSubsystem) {
+        super(() -> {
+            getSequence(rollerSubsystem, tiltedElevatorSubsystem).schedule();
+        }, rollerSubsystem, tiltedElevatorSubsystem);
     }
 
-    public static Command choose(TiltedElevatorSubsystem tiltedElevatorSubsystem, RollerSubsystem rollerSubsystem){
+    /**
+     * Gets the correct drop sequence to schedule for this command.
+     * @param rollerSubsystem The roller subsystem.
+     * @param tiltedElevatorSubsystem The tilter elevator subsystem.
+     * @return The `SequentialCommandGroup` representing dropping the piece as a sequence.
+     */
+    private static SequentialCommandGroup getSequence(
+        RollerSubsystem rollerSubsystem, TiltedElevatorSubsystem tiltedElevatorSubsystem
+    ) {
         ElevatorState state = tiltedElevatorSubsystem.getState();
-        if(state == ElevatorState.CONE_HIGH){
-            return(new DropSequence(rollerSubsystem, tiltedElevatorSubsystem, 
-            ElevatorState.CONE_HIGH_DROP, .2, .2));
-        }
-        if(state == ElevatorState.CONE_MID){
-            return(new DropSequence(rollerSubsystem, tiltedElevatorSubsystem,
-             ElevatorState.CONE_MID_DROP, .2, .2));
-        }
 
-        return(new DropSequence(rollerSubsystem, tiltedElevatorSubsystem, state, 0, 0));
+        return switch (state) {
+            case CONE_HIGH -> new DropSequence(rollerSubsystem, tiltedElevatorSubsystem, ElevatorState.CONE_HIGH_DROP, .2, .2);
+            case CONE_MID -> new DropSequence(rollerSubsystem, tiltedElevatorSubsystem, ElevatorState.CONE_MID_DROP, .2, .2);
+            default -> new DropSequence(rollerSubsystem, tiltedElevatorSubsystem, state, 0, 0);
+        };
     }
 }
