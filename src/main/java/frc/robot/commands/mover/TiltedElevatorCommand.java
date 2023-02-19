9fb399bb2945ce7be238dc883781845c5dc93ebb
penguin212 <dhuber06@gmail.com>
2023-02-19 12:18:32 -0800

    Tilted Elevator command max duration implemented

--- src/main/java/frc/robot/commands/mover/TiltedElevatorCommand.java
+++ src/main/java/frc/robot/commands/mover/TiltedElevatorCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.mover;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.subsystems.TiltedElevatorSubsystem;
@@ -8,16 +9,20 @@ import frc.robot.subsystems.TiltedElevatorSubsystem.ElevatorState;
 public class TiltedElevatorCommand extends CommandBase {
     private final TiltedElevatorSubsystem tiltedElevatorSubsystem;
     private final ElevatorState targetState;
+    private final Timer timer;
 
     public TiltedElevatorCommand(TiltedElevatorSubsystem tiltedElevatorSubsystem, ElevatorState targetState) {
         this.tiltedElevatorSubsystem = tiltedElevatorSubsystem;
         this.targetState = targetState;
+        timer = new Timer();
+
         addRequirements(tiltedElevatorSubsystem);
     }
 
     public void initialize(){
         System.out.println("Mover to " + targetState);
         tiltedElevatorSubsystem.setState(targetState);
+        timer.start();
     }
 
     public void end(boolean interrupted) {
@@ -25,6 +30,6 @@ public class TiltedElevatorCommand extends CommandBase {
     }
     
     public boolean isFinished() {
-        return tiltedElevatorSubsystem.atTarget();
+        return tiltedElevatorSubsystem.atTarget() || timer.hasElapsed(1);
     }
 }
