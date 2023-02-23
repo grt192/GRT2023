82b2eeb9ca17cd991c551545614908f160d1e99f
= <=>
2023-02-22 17:42:12 -0800

    created code for command to go over charging station during auton

---
+++ src/main/java/frc/robot/commands/Balancing/GoOverCommand.java
@@ -0,0 +1,81 @@
+package frc.robot.commands.Balancing;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.drivetrain.BaseDrivetrain;
+import edu.wpi.first.wpilibj.Timer;
+
+public class GoOverCommand extends CommandBase {
+    private final BaseDrivetrain driveSubsystem;
+    private final AHRS ahrs; 
+
+    private double returnDrivePower; // drive power to be returned to DT
+    private double returnAngularPower; // angular power to return to DT (for heading correction)
+
+    public boolean reachedStation;
+    public boolean passedCenter;
+    public boolean overStation;
+    public boolean waiting;
+
+    private final Timer timer;
+
+    public GoOverCommand(BaseDrivetrain driveSubsystem) {
+        this.driveSubsystem = driveSubsystem;
+        this.ahrs = driveSubsystem.getAhrs();
+
+        timer = new Timer();
+        addRequirements(driveSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        reachedStation = false;
+        passedCenter = false;
+        overStation = false;
+        waiting = false;
+
+    }
+
+    @Override
+    public void execute() {
+        
+        if(!reachedStation) {
+            returnDrivePower = - 0.80;
+            if(ahrs.getPitch() <= - 15.0) reachedStation = true;
+        }
+
+        if(reachedStation && !passedCenter){
+            returnDrivePower = 0.5;
+            if(ahrs.getPitch() >= -0.0){
+                returnDrivePower = 0.4;
+                passedCenter = true;
+            } 
+        }
+        if(reachedStation && passedCenter && !overStation){
+            if(Math.abs(ahrs.getPitch()) <= 0.5) {
+                overStation = true;
+            }
+            else returnDrivePower = 0.4;
+        }
+
+        if(overStation && !waiting){
+            timer.reset();
+            timer.start();
+            returnDrivePower = 0.4;
+            waiting = true;
+        }
+
+        driveSubsystem.setDrivePowers(returnDrivePower);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return overStation && timer.hasElapsed(0.5);
+    }
+}
