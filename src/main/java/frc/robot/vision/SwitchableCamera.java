49521be6fa33aaa4262b027d5a5ab07c5878618c
Alina Li <alina.li@outlook.com>
2023-02-23 20:25:13 -0800

    set raw exposure on cams

--- src/main/java/frc/robot/vision/SwitchableCamera.java
+++ src/main/java/frc/robot/vision/SwitchableCamera.java
@@ -17,6 +17,12 @@ public class SwitchableCamera {
     private final UsbCamera front;
     private final UsbCamera back;
 
+    private static final int FRONT_EXPOSURE = 20;
+    private static final int FRONT_BRIGHTNESS = 80;
+
+    private static final int BACK_EXPOSURE = 40;
+    private static final int BACK_BRIGHTNESS = 80;
+
     private final VideoSink server;
     private final ComplexWidget widget;
 
@@ -32,11 +38,19 @@ public class SwitchableCamera {
         front.setFPS(30);
         back.setFPS(30);
 
-        front.setExposureManual(20);
-        back.setExposureManual(40);
+        // front.setExposureManual(FRONT_EXPOSURE);
+        // back.setExposureManual(BACK_EXPOSURE);
 
-        front.setBrightness(40);
-        back.setBrightness(60);
+        //https://github.com/wpilibsuite/allwpilib/blob/main/cscore/src/main/native/linux/UsbCameraImpl.cpp#L108-L124
+        // https://www.chiefdelphi.com/t/usb-camera-exposure-too-high-with-setexposuremanual/353630/8
+        front.getProperty("raw_exposure_absolute").set(10);
+        back.getProperty("raw_exposure_absolute").set(10);
+
+        // front.getProperty("raw_brightness").set(40);
+        // back.getProperty("raw_exposure_absolute").set(40);
+
+        front.setBrightness(FRONT_BRIGHTNESS);
+        back.setBrightness(BACK_BRIGHTNESS);
 
         server = CameraServer.getServer();
 
