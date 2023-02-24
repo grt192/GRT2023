ee7f69acdd8c7e01b2fc5c45736f140ccfb34fd1
Ethan Liang <e3lcool@gmail.com>
2023-02-24 13:57:58 -0800

    switch camera on elevator state change

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -4,6 +4,7 @@ import static frc.robot.Constants.RollerConstants.ALLOW_OPEN_HEIGHT;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.RollerSubsystem.HeldPiece;
+import frc.robot.subsystems.TiltedElevatorSubsystem.ElevatorState;
 import frc.robot.vision.SwitchableCamera;
 
 public class Superstructure extends SubsystemBase {
@@ -11,7 +12,7 @@ public class Superstructure extends SubsystemBase {
     private final TiltedElevatorSubsystem tiltedElevatorSubsystem;
     private final SwitchableCamera switchableCamera;
 
-    private boolean lastHasPiece = false;
+    private ElevatorState lastState;
 
     public Superstructure(RollerSubsystem rollerSubsystem, TiltedElevatorSubsystem tiltedElevatorSubsystem, SwitchableCamera switchableCamera) {
         this.rollerSubsystem = rollerSubsystem;
@@ -22,13 +23,25 @@ public class Superstructure extends SubsystemBase {
     @Override
     public void periodic() {
         boolean hasPiece = rollerSubsystem.getPiece() != HeldPiece.EMPTY;
-
         tiltedElevatorSubsystem.pieceGrabbed = hasPiece;
-        if (hasPiece != lastHasPiece) {
-            switchableCamera.setCamera(!hasPiece);
-        }
 
-        lastHasPiece = hasPiece;
+        ElevatorState currentState = tiltedElevatorSubsystem.getState();
+        if (currentState != lastState) {
+            switch (currentState) {
+                case CONE_HIGH:
+                case CONE_MID:
+                case CUBE_MID:
+                case CUBE_HIGH:
+                    switchableCamera.setCamera(false);
+                    break;
+                case GROUND:
+                    switchableCamera.setCamera(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+        lastState = currentState;
 
         rollerSubsystem.allowOpen = tiltedElevatorSubsystem.getExtension() >= ALLOW_OPEN_HEIGHT;
     }
