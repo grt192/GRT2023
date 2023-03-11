0cbaaada3116f469ab1eaf952ca0749efd15a743
penguin212 <dhuber06@gmail.com>
2023-03-10 22:51:22 -0800

    continuous color mode

--- src/main/java/frc/robot/subsystems/leds/LEDStrip.java
+++ src/main/java/frc/robot/subsystems/leds/LEDStrip.java
@@ -2,11 +2,13 @@ package frc.robot.subsystems.leds;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class LEDStrip {
     private final AddressableLED led;
     private final AddressableLEDBuffer ledBuffer;
+    private final Timer ledTimer;
 
     public LEDStrip(int ledPort, int ledLength) {
         led = new AddressableLED(ledPort);
@@ -14,6 +16,8 @@ public class LEDStrip {
 
         led.setLength(ledBuffer.getLength());
         led.start();
+        ledTimer = new Timer();
+        ledTimer.start();
     }
 
     /**
@@ -25,5 +29,17 @@ public class LEDStrip {
             ledBuffer.setLED(i, color);
         }
         led.setData(ledBuffer);
+    }
+
+    public void setContinuousColor(Color color){
+        if(ledTimer.hasElapsed(0.1)){
+            for (int i = ledBuffer.getLength() - 1; i > 0; i--){
+                ledBuffer.setLED(i, ledBuffer.getLED(i - 1));
+            }
+            ledBuffer.setLED(0, color);
+            led.setData(ledBuffer);
+            ledTimer.reset();
+            ledTimer.start();
+        }
     }
 }
