6a7294fa3e116402ae9144869ec6c629d46ae933
Kevin Yu <kevinyu567@gmail.com>
2023-03-26 16:57:01 -0700

    Don't raise elevator until angle is aligned

--- src/main/java/frc/robot/commands/dropping/AlignToNodeCommand.java
+++ src/main/java/frc/robot/commands/dropping/AlignToNodeCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands.dropping;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 import frc.robot.commands.swerve.GoToPointCommand;
 import frc.robot.positions.PlacePosition;
@@ -9,6 +10,8 @@ import frc.robot.subsystems.drivetrain.BaseSwerveSubsystem;
 import frc.robot.subsystems.tiltedelevator.TiltedElevatorSubsystem;
 
 public class AlignToNodeCommand extends ParallelCommandGroup {
+    private static final double ELEVATOR_TOLERANCE_RADS = Math.toRadians(10.0);
+
     /**
      * Creates an align-to-node command from a given swerve subsystem, elevator subsystem, target place position, and
      * whether the robot is currently on the red team.
@@ -22,9 +25,13 @@ public class AlignToNodeCommand extends ParallelCommandGroup {
         BaseSwerveSubsystem swerveSubsystem, TiltedElevatorSubsystem tiltedElevatorSubsystem,
         PlacePosition targetPlacePosition, boolean isRed
     ) {
+        GoToPointCommand driveCommand = new GoToPointCommand(swerveSubsystem, targetPlacePosition.alignPosition.getPose(isRed));
+
         addCommands(
-            new InstantCommand(() -> tiltedElevatorSubsystem.setState(targetPlacePosition.elevatorState), tiltedElevatorSubsystem),
-            new GoToPointCommand(swerveSubsystem, targetPlacePosition.alignPosition.getPose(isRed))
+            driveCommand,
+            new WaitUntilCommand(() -> driveCommand.isHeadingAligned(ELEVATOR_TOLERANCE_RADS)).andThen(
+                new InstantCommand(() -> tiltedElevatorSubsystem.setState(targetPlacePosition.elevatorState), tiltedElevatorSubsystem)
+            )
         );
     }
 }
